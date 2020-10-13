3a9c09e8bd3c698204cc59582a9f31fb4965c39a
abernier <antoine.bernier@gmail.com>
2020-10-13 15:16:34 +0200

    indentation

--- README.md
+++ README.md
@@ -7,16 +7,16 @@ see: `index.html`
 ```html
 <section style="display:flex; height:200vh;">
   <div data-parallax-dy="0">A</div>
-    <div data-parallax-dy=".1">A</div>
-    <div data-parallax-dy=".2">B</div>
-    <div data-parallax-dy=".3">C</div>
-    <div data-parallax-dy=".4">D</div>
-    <div data-parallax-dy=".5">E</div>
-    <div data-parallax-dy=".6">F</div>
-    <div data-parallax-dy=".7">G</div>
-    <div data-parallax-dy=".8">H</div>
-    <div data-parallax-dy=".9">I</div>
-    <div data-parallax-dy>J</div>
+  <div data-parallax-dy=".1">A</div>
+  <div data-parallax-dy=".2">B</div>
+  <div data-parallax-dy=".3">C</div>
+  <div data-parallax-dy=".4">D</div>
+  <div data-parallax-dy=".5">E</div>
+  <div data-parallax-dy=".6">F</div>
+  <div data-parallax-dy=".7">G</div>
+  <div data-parallax-dy=".8">H</div>
+  <div data-parallax-dy=".9">I</div>
+  <div data-parallax-dy>J</div>
 </section>
 
 <script type="module">
--- index.js
+++ index.js
@@ -9,28 +9,28 @@ const TRANSFORMPROP = ["transform", "webkitTransform", "MozTransform", "msTransf
 let lastScrollY = window.pageYOffset
 function onScroll() {
   requestTick()
-
+  
   lastScrollY = window.pageYOffset
 }
 
 let ticking = false
 function requestTick() {
   if (!ticking) {
-      ticking = true
-      requestAnimationFrame(update)
+    ticking = true
+    requestAnimationFrame(update)
   }
 }
 function update() {
   var l = instances.length
-
+  
   var instance
   while (l--) {
-  instance = instances[l]
-
-  var percent = instance.percent(lastScrollY)
-  instance.draw(percent)
+    instance = instances[l]
+    
+    var percent = instance.percent(lastScrollY)
+    instance.draw(percent)
   }
-
+  
   // allow further rAFs to be called
   ticking = false
 }
@@ -44,25 +44,25 @@ const instances = []
 
 function Parallax(el, options = {}) {
   this.el = el
-
+  
   this.amplitude = options.amplitude || 1
-
+  
   // Add .parallax class
   this.el.classList.add('parallax')
-
+  
   // setDims
   this.setDims()
-
+  
   //
   // initial draw
   //
-
+  
   var percent = this.percent(lastScrollY)
   
   this.setItems()
-
+  
   this.draw(percent)
-
+  
   instances.push(this)
 }
 Parallax.prototype.setItems = function () {
@@ -70,36 +70,36 @@ Parallax.prototype.setItems = function () {
 }
 Parallax.prototype.setDims = function () {
   //console.log('setDims')
-
+  
   const rect = this.el.getBoundingClientRect()
-      
+  
   this.offsettop = rect.top + window.pageYOffset - document.documentElement.clientTop  // .offset() equivalent, see: https://stackoverflow.com/a/28857255/133327
   this.WH = window.innerHeight
   this.h = rect.height
 };
 Parallax.prototype.percent = function (scrolltop = window.pageYOffset) {
   var percent = ((this.offsettop - scrolltop) + this.h) / (this.WH + this.h)
-
+  
   return percent
 };
 Parallax.prototype.draw = function (percent = this.percent()) {
   if (percent > 0 && percent < 1) {
-      this.items.forEach(el  => {
-
-          const dy = +(el.dataset.parallaxDy || this.amplitude)
-          console.log('dy=', dy)
-
-          const ty = 100*(2*(percent - .5) * dy)
-          
-          let t
-          if (Parallax.has3d) {
-              t = 'translate3d(0,' + ty + '%,0)'
-          } else {
-              t = 'translate(0,' + ty + '%)'
-          }
-              
-          el.style[TRANSFORMPROP] = t
-      })
+    this.items.forEach(el  => {
+      
+      const dy = +(el.dataset.parallaxDy || this.amplitude)
+      console.log('dy=', dy)
+      
+      const ty = 100*(2*(percent - .5) * dy)
+      
+      let t
+      if (Parallax.has3d) {
+        t = 'translate3d(0,' + ty + '%,0)'
+      } else {
+        t = 'translate(0,' + ty + '%)'
+      }
+      
+      el.style[TRANSFORMPROP] = t
+    })
   }
 }
 
@@ -108,25 +108,25 @@ function has3d() {
   var el = document.createElement('p')
   var has3d
   var transforms = {
-      'webkitTransform':'-webkit-transform',
-      'OTransform':'-o-transform',
-      'msTransform':'-ms-transform',
-      'MozTransform':'-moz-transform',
-      'transform':'transform'
+    'webkitTransform':'-webkit-transform',
+    'OTransform':'-o-transform',
+    'msTransform':'-ms-transform',
+    'MozTransform':'-moz-transform',
+    'transform':'transform'
   }
-
+  
   // Add it to the body to get the computed style.
   document.body.insertBefore(el, null)
-
+  
   for (var t in transforms) {
-      if (el.style[t] !== undefined) {
-          el.style[t] = "translate3d(1px,1px,1px)"
-          has3d = window.getComputedStyle(el).getPropertyValue(transforms[t])
-      }
+    if (el.style[t] !== undefined) {
+      el.style[t] = "translate3d(1px,1px,1px)"
+      has3d = window.getComputedStyle(el).getPropertyValue(transforms[t])
+    }
   }
-
+  
   document.body.removeChild(el)
-
+  
   return (has3d !== undefined && has3d.length > 0 && has3d !== "none")
 }
 Parallax.has3d = has3d()
